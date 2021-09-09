9d2dba346ce960b5720bffa5efa94a4b25ffcbe5
Lydia Lima <62971275+lydialima12@users.noreply.github.com>
2021-09-08 21:08:57 -0300

    Iniciando o projeto
    Inicializando o Projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      <header className="App-header">
+        <p>Este é meu componente container</p>
+      </header>
+    </main>
+  );
+}
+
+export default App;
